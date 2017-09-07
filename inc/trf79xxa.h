654e9fcb648fb8cd0f9941bc35e5987e18fb191c
phreaknik <johnboydiv@gmail.com>
2017-09-06 23:24:18 -0500

    Fix BIT0 - BITF definitions

--- inc/trf79xxa.h
+++ inc/trf79xxa.h
@@ -39,7 +39,7 @@
 #ifndef _TRF79xxa_H_
 #define _TRF79xxa_H_
 
-//=====Driver Configuration======================================
+//==== Driver Configuration =====================================
 
 #include <trf79xxa_driver_config.h>
 #include "../src/types.h"
@@ -91,7 +91,7 @@
 
 //===============================================================
 
-//=====TRF79xxA API Types========================================
+//==== TRF79xxA API Types =======================================
 
 /*
  * Power Settings for TRF79xxA.
@@ -145,7 +145,25 @@ typedef enum
 
 //==== TRF797x Definitions ======================================
 
-//---- Direct commands ------------------------------------------
+//---- Standard Bits --------------------------------------------
+#define BIT0                                            (0x0001)
+#define BIT1                                            (0x0002)
+#define BIT2                                            (0x0004)
+#define BIT3                                            (0x0008)
+#define BIT4                                            (0x0010)
+#define BIT5                                            (0x0020)
+#define BIT6                                            (0x0040)
+#define BIT7                                            (0x0080)
+#define BIT8                                            (0x0100)
+#define BIT9                                            (0x0200)
+#define BITA                                            (0x0400)
+#define BITB                                            (0x0800)
+#define BITC                                            (0x1000)
+#define BITD                                            (0x2000)
+#define BITE                                            (0x4000)
+#define BITF                                            (0x8000)
+
+//---- Direct Commands ------------------------------------------
 #define TRF79XXA_IDLE_CMD                               0x00
 #define TRF79XXA_SOFT_INIT_CMD                          0x03
 #if (TRF79xxA_VERSION == 70)
@@ -215,7 +233,7 @@ typedef enum
 
 //===============================================================
 
-//=====TRF79xxA API Functions====================================
+//==== TRF79xxA API Functions ===================================
 
 void TRF79xxA_disableSlotCounter(void);
 void TRF79xxA_enableSlotCounter(void);
@@ -251,7 +269,7 @@ bool TRF79xxA_checkExternalRfField(void);
 
 //===============================================================
 
-//=====TRF79xxA API Externs======================================
+//==== TRF79xxA API Externs =====================================
 
 extern void trf79xxaSpi_directCommand(uint8_t cmd);
 extern void trf79xxaSpi_setup();
